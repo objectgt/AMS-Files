0d400ff3a2f2031210e971c0adf75da76a2712bb
AutoReportSystem <fellowhuman426@gmail.com>
2026-03-06 23:53:28 -0800

    Refactor ARSComponent to ARS with async player ID fetch

--- COPY.cs
+++ COPY.cs
@@ -1,9 +1,54 @@
 namespace ARS
 {
-    internal class ARSComponent : MonoBehaviour
+    internal class ARS : MonoBehaviourPunCallbacks
     {
+        #region PhotonOverrides
+
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
+
+            try
+            {
+                StartCoroutine(DelayedCheckServer());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        IEnumerator DelayedCheckServer()
+        {
+            yield return new WaitForSeconds(2.5f);
+            CheckServer();
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
+
+            try
+            {
+                CheckUser(newPlayer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        #endregion
+
+        #region Main
+
         void Start()
         {
+            if (!Directory.Exists("ARS"))
+                Directory.CreateDirectory("ARS");
+
+            _ = AsyncGetPlayerIDs();
+
             EasierLog("ARS fully initialized, thank you for helping the gorilla tag modding community!");
         }
 
@@ -12,60 +57,99 @@ namespace ARS
         public static string[] PlayersToReport = new string[0];
         static bool HasChecked = false;
 
+        static string LastRoomChecked = string.Empty;
+
         void Update()
         {
-            if (HasChecked && !PhotonNetwork.InRoom)
-            {
-                HasChecked = false;
-                PlayersChecked.Clear();
-            }
-
-            if (PlayerIDs == string.Empty)
-                GetPlayerIDs();
-
-            if (PlayersToReport.Length < 3)
-                GetPlayerIDs();
-
-            if (PlayersToReport == null) return;
-
-            foreach (Player plr in PhotonNetwork.PlayerListOthers)
-            {
-                if (PlayersChecked.Contains(plr)) continue;
-
-                if (NeedToReport(plr))
+            if (PhotonNetwork.InRoom)
+                if (HasChecked && PhotonNetwork.CurrentRoom.Name != LastRoomChecked)
                 {
-                    GorillaPlayerScoreboardLine.ReportPlayer(plr.UserId, GorillaPlayerLineButton.ButtonType.Toxicity, plr.NickName);
-                    EasierLog($"Reported user {plr.NickName}.");
+                    HasChecked = false;
+                    PlayersChecked.Clear();
                 }
-
-                PlayersChecked.Add(plr);
-            }
-
-            if (!HasChecked && PhotonNetwork.InRoom)
-                HasChecked = true;
         }
 
+        static void CheckServer()
+        {
+            if (PlayersToReport.Length == 0) return;
+
+            if (PhotonNetwork.InRoom)
+                foreach (Player plr in PhotonNetwork.PlayerListOthers)
+                {
+                    if (PlayersChecked.Contains(plr)) continue;
+
+                    if (NeedToReport(plr))
+                    {
+                        GorillaPlayerScoreboardLine.ReportPlayer(plr.UserId,
+                            GorillaPlayerLineButton.ButtonType.Toxicity, plr.NickName);
+                        EasierLog($"Reported user {plr.NickName}.");
+                    }
+
+                    PlayersChecked.Add(plr);
+                }
+
+            if (!HasChecked && PhotonNetwork.InRoom)
+            {
+                LastRoomChecked = PhotonNetwork.CurrentRoom.Name;
+                HasChecked = true;
+            }
+        }
+
+        static void CheckUser(Player plrToCheck)
+        {
+            if (!PlayersChecked.Contains(plrToCheck) && NeedToReport(plrToCheck))
+            {
+                GorillaPlayerScoreboardLine.ReportPlayer(plrToCheck.UserId,
+                    GorillaPlayerLineButton.ButtonType.Toxicity, plrToCheck.NickName);
+
+                EasierLog($"Reported user {plrToCheck.NickName}.");
+                PlayersChecked.Add(plrToCheck);
+            }
+        }
+
+        [Obsolete("Use async version instead. This blocks main thread.")]
         static void GetPlayerIDs()
         {
             if (PlayerIDs == string.Empty)
-                PlayerIDs = new WebClient().DownloadString("https://raw.githubusercontent.com/AutoReportSystem/ARSPlayerIDs/refs/heads/main/Player%20Ids.txt").Trim();
+                PlayerIDs = new WebClient()
+                    .DownloadString(
+                        "https://raw.githubusercontent.com/AutoReportSystem/ARSPlayerIDs/refs/heads/main/Player%20Ids.txt")
+                    .Trim();
 
-            PlayersToReport = PlayerIDs.Split(',').ToArray();
+            PlayersToReport = PlayerIDs.Split(',');
 
             EasierLog($"Recieved player ids to report. Count of users: {PlayersToReport.Count()}");
         }
 
-        static bool NeedToReport(Player plr)
+        private static readonly HttpClient Client = new HttpClient();
+
+        static async Task AsyncGetPlayerIDs()
         {
+            PlayerIDs = await Client.GetStringAsync(
+                "https://raw.githubusercontent.com/AutoReportSystem/ARSPlayerIDs/refs/heads/main/Player%20Ids.txt");
+            PlayerIDs = PlayerIDs.Trim();
+
+            PlayersToReport = PlayerIDs.Split(',');
+
+            EasierLog($"Recieved player ids to report. Count of users: {PlayersToReport.Count()}");
+        }
+
+        public static bool NeedToReport(Player plr)
+        {
+            if (plr == null)
+                return false;
+
             for (int i = 0; i < PlayersToReport.Length; i++)
                 if (PlayersToReport[i] == plr.UserId)
                     return true;
 
             return false;
         }
+
         static void EasierLog(string message)
         {
             Console.WriteLine($"[ARS LOGGING] {message}");
         }
-    }
+
+        #endregion
 }
