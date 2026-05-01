bdb696388b5cf56074caf9d137a541be227ce614
object gt <averypaxton5716@gmail.com>
2026-05-01 07:09:02 -0400

    Refactor AMS class to use MonoBehaviourPunCallbacks

--- COPY.cs
+++ COPY.cs
@@ -1,151 +1,134 @@
-using BepInEx;
-using Photon.Pun;
-using Photon.Realtime;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using UnityEngine;
-
-namespace AMS;
-
-[BepInIncompatibility("industry.resurgencev2")]
-[BepInPlugin("com.industry.objectgt.automutesys", "Automatic Muting System", "1.0.1")]
-internal class AMS : BaseUnityPlugin
+namespace AMS
 {
-    #region Main
-
-    void Start()
+    internal class AMS : MonoBehaviourPunCallbacks
     {
-        this.AddComponent<PhotonCallbacks>();
+        #region PhotonOverrides
 
-        if (!Directory.Exists("AMS"))
-            Directory.CreateDirectory("AMS");
+        public override void OnJoinedRoom()
+        {
+            base.OnJoinedRoom();
 
-        _ = AsyncGetPlayerIDs();
-
-        EasierLog("AMS fully initialized, thank you for helping the gorilla tag peace & anti-toxicity community!");
-    }
-
-    public static List<Player> PlayersChecked = new();
-    public static string PlayerIDs = string.Empty;
-    public static HashSet<string> PlayersToMute = new();
-    static bool HasChecked;
-
-    static string LastRoomChecked = string.Empty;
-
-    void Update()
-    {
-        if (PhotonNetwork.InRoom)
-            if (HasChecked && PhotonNetwork.CurrentRoom.Name != LastRoomChecked)
+            try
             {
-                HasChecked = false;
-                PlayersChecked.Clear();
+                StartCoroutine(DelayedCheckServer());
             }
-    }
-
-    public static void CheckServer()
-    {
-        if (PlayersToMute.Count == 0) return;
-
-        if (PhotonNetwork.InRoom)
-            foreach (Player plr in PhotonNetwork.PlayerListOthers)
-                CheckUser(plr);
-
-        if (!HasChecked && PhotonNetwork.InRoom)
-        {
-            LastRoomChecked = PhotonNetwork.CurrentRoom.Name;
-            HasChecked = true;
-        }
-    }
-
-    public static void CheckUser(Player plrToCheck)
-    {
-        if (!PlayersChecked.Contains(plrToCheck) && NeedToMute(plrToCheck))
-        {
-            foreach (GorillaPlayerScoreboardLine scoreboardLine in
-                     GorillaScoreboardTotalUpdater.allScoreboardLines.Where(scoreboardLine =>
-                         scoreboardLine.linePlayer ==
-                         NetworkSystem.Instance.GetNetPlayerByID(plrToCheck.ActorNumber)))
+            catch (Exception e)
             {
-                scoreboardLine.PressButton(true, GorillaPlayerLineButton.ButtonType.Mute);
+                Console.WriteLine(e);
             }
-
-            EasierLog($"Muted user {plrToCheck.NickName}.");
-            PlayersChecked.Add(plrToCheck);
         }
-    }
 
-    private static readonly HttpClient Client = new();
-
-    static async Task AsyncGetPlayerIDs()
-    {
-        PlayerIDs = await Client.GetStringAsync(
-            "https://www.objectgt.org/api/serverdata/AMS");
-        PlayerIDs = PlayerIDs.Trim();
-
-        PlayersToMute = PlayerIDs.Split(",").Select(id => id.Trim())
-            .Where(id => !id.IsNullOrEmpty()).ToHashSet();
-
-        EasierLog($"Recieved player ids to mute. Count of users: {PlayersToMute.Count()}");
-    }
-
-    public static bool NeedToMute(Player plr)
-    {
-        if (plr == null)
-            return false;
-
-        return PlayersToMute.Contains(plr.UserId);
-    }
-
-    static void EasierLog(string message)
-    {
-        Console.WriteLine($"[AMS LOGGING] {message}");
-    }
-
-    #endregion
-}
-
-public class PhotonCallbacks : MonoBehaviourPunCallbacks
-{
-    #region PhotonOverrides
-
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-
-        try
+        IEnumerator DelayedCheckServer()
         {
-            StartCoroutine(DelayedCheckServer());
+            yield return new WaitForSeconds(UnityEngine.Random.Range(2.5f, 10f));
+            CheckServer();
         }
-        catch (Exception e)
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
         {
-            Console.WriteLine(e);
+            base.OnPlayerEnteredRoom(newPlayer);
+
+            try
+            {
+                CheckUser(newPlayer);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
-    }
 
-    IEnumerator DelayedCheckServer()
-    {
-        yield return new WaitForSeconds(UnityEngine.Random.Range(2.5f, 10f));
-        AMS.CheckServer();
-    }
+        #endregion
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        base.OnPlayerEnteredRoom(newPlayer);
+        #region Main
 
-        try
+        void Start()
         {
-            AMS.CheckUser(newPlayer);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
-    }
+            if (!Directory.Exists("AMS"))
+                Directory.CreateDirectory("AMS");
 
-    #endregion
+            _ = AsyncGetPlayerIDs();
+
+            EasierLog("AMS fully initialized, thank you for helping the gorilla tag peace & anti-toxicity community!");
+        }
+
+        public static List<Player> PlayersChecked = new List<Player>();
+        public static string PlayerIDs = string.Empty;
+        public static HashSet<string> PlayersToMute = new();
+        static bool HasChecked = false;
+
+        static string LastRoomChecked = string.Empty;
+
+        void Update()
+        {
+
+            if (PhotonNetwork.InRoom)
+                if (HasChecked && PhotonNetwork.CurrentRoom.Name != LastRoomChecked)
+                {
+                    HasChecked = false;
+                    PlayersChecked.Clear();
+                }
+        }
+
+        static void CheckServer()
+        {
+            if (PlayersToMute.Count == 0) return;
+
+            if (PhotonNetwork.InRoom)
+                foreach (Player plr in PhotonNetwork.PlayerListOthers)
+                    CheckUser(plr);
+
+            if (!HasChecked && PhotonNetwork.InRoom)
+            {
+                LastRoomChecked = PhotonNetwork.CurrentRoom.Name;
+                HasChecked = true;
+            }
+        }
+
+        static void CheckUser(Player plrToCheck)
+        {
+            if (!PlayersChecked.Contains(plrToCheck) && NeedToMute(plrToCheck))
+            {
+                foreach (GorillaPlayerScoreboardLine scoreboardLine in
+                         GorillaScoreboardTotalUpdater.allScoreboardLines.Where(scoreboardLine =>
+                             scoreboardLine.linePlayer ==
+                             NetworkSystem.Instance.GetNetPlayerByID(plrToCheck.ActorNumber)))
+                {
+                    scoreboardLine.PressButton(true, GorillaPlayerLineButton.ButtonType.Mute);
+                }
+
+                EasierLog($"Muted user {plrToCheck.NickName}.");
+                PlayersChecked.Add(plrToCheck);
+            }
+        }
+
+        private static readonly HttpClient Client = new HttpClient();
+
+        static async Task AsyncGetPlayerIDs()
+        {
+            PlayerIDs = await Client.GetStringAsync(
+                "https://www.objectgt.org/api/serverdata/AMS");
+            PlayerIDs = PlayerIDs.Trim();
+
+            PlayersToMute = PlayerIDs.Split(",").Select(id => id.Trim())
+                .Where(id => !id.IsNullOrEmpty()).ToHashSet();
+
+            EasierLog($"Recieved player ids to mute. Count of users: {PlayersToMute.Count()}");
+        }
+
+        public static bool NeedToMute(Player plr)
+        {
+            if (plr == null)
+                return false;
+
+            return PlayersToMute.Contains(plr.UserId);
+        }
+
+        static void EasierLog(string message)
+        {
+            Console.WriteLine($"[AMS LOGGING] {message}");
+        }
+
+        #endregion
+    }
 }
